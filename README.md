ee778186f02f180c41fead5ddf8624ba69ce487b
Maxim Abylkassov <m.abylkassov@gmail.com>
2024-05-29 15:10:02 +0500

    29/05/2024 15:10 def profile - added

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ python -m venv venv
 
 pip -m install flask
 
+Commits
+
 29/05/2024 11:33 Work with decorators
 
 29/05/2024 11:39 render_template
@@ -13,3 +15,5 @@ pip -m install flask
 29/05/2024 11:54 title, menu - added
 
 29/05/2024 14:58 url_for - added
+
+29/05/2024 15:10 def profile - added
--- main.py
+++ main.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 menu = ["Index", "First App", "Contacts"]
 
 
-
-@app.route("/index")
 @app.route("/")
 def index():
     print( url_for('index') )
@@ -16,6 +14,13 @@ def index():
 def about():
     print( url_for('about') )
     return render_template('index.html')
+
+@app.route("/profile/<username>")
+def profile(username):
+    return f"User: {username}"
+
+""" with app.test_request_context():
+    print( url_for('about') ) """
 
 if __name__ == "__main__":
     app.run(debug=True)
