bac0a10d8cded79366013e0996decb92de9fa463
Maxim Abylkassov <m.abylkassov@gmail.com>
2024-05-29 11:41:15 +0500

    29/05/2024 11:41 Bootstrap, templates, index.html

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ pip -m install flask
 29/05/2024 11:33 Work with decorators
 
 29/05/2024 11:39 render_template
+
+29/05/2024 11:41 Bootstrap, templates, index.html
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/index")
 @app.route("/")
 def index():
-    return "index"
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
