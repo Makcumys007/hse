42e9441295f29341c9a4e48daaea1e869f30c5f0
Maxim Abylkassov <m.abylkassov@gmail.com>
2024-05-29 11:54:09 +0500

    29/05/2024 11:54 title, menu - added

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ pip -m install flask
 29/05/2024 11:39 render_template
 
 29/05/2024 11:41 Bootstrap, templates, index.html
+
+29/05/2024 11:54 title, menu - added
--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+menu = ["Index", "First App", "Contacts"]
+
+
+
 @app.route("/index")
 @app.route("/")
 def index():
-    return render_template('index.html')
+
+    return render_template('index.html', title="Index", menu=menu)
 
 @app.route("/about")
 def about():
-    return "<h1>About</h1>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
