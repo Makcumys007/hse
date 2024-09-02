3029e65deabf01a58f1dd1ffbb04f91ec974dced
Maxim Abylkassov <maxim.abylkassov@gmail.com>
2024-09-02 13:52:35 +0000

    HSEBoard  Information

--- resources/views/admin/hseboard.blade.php
+++ resources/views/admin/hseboard.blade.php
@@ -10,6 +10,15 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                 <section>
+                    <header>
+                        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                            HSEBoard  Information
+                        </h2>
+
+                        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                            Update information.
+                        </p>
+                    </header>
                     <form method="post" action="" class="mt-6 space-y-6">       
                         <div>
                             <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" >Lost time Injuries</label>
@@ -37,7 +46,7 @@
                         </div>
 
                         <div class="flex items-center gap-4">
-                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">            Save</button>
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"> Save</button>
 
                         </div>
                     </form>
