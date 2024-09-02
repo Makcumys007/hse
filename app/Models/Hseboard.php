68e701573c66efff615225a9fd14197c77909343
Maxim Abylkassov <maxim.abylkassov@gmail.com>
2024-09-02 08:41:26 +0000

    protected $fillable

--- app/Models/Hseboard.php
+++ app/Models/Hseboard.php
@@ -10,4 +10,11 @@ class Hseboard extends Model
     use HasFactory;
 
     protected $table = 'hseboard';
+
+    protected $fillable = ['lost_time_injuries',
+                            'medical_treatment',
+                            'first_aid_cases',
+                            'lost_time_injuries_free_days',
+                            'safe_men_hours',
+                            'date'];
 }
