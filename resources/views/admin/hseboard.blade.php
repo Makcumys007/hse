c7646cf5322a2d4d57a18bcb94922d23d574bc83
Maxim Abylkassov <maxim.abylkassov@gmail.com>
2024-09-02 13:49:02 +0000

    Form for hseboard created

--- resources/views/admin/hseboard.blade.php
+++ resources/views/admin/hseboard.blade.php
@@ -9,7 +9,39 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                <section>
+                    <form method="post" action="" class="mt-6 space-y-6">       
+                        <div>
+                            <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" >Lost time Injuries</label>
+                            <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full"  name="lost_time_injuries" type="number"  required="required"  >
+                        </div>
+
+                        <div>
+                            <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" >Medical Treatment</label>
+                                <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full" name="medical_treatment" type="number" required="required" >            
+                        </div>
+
+                        <div>
+                            <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" >First Aid Cases</label>
+                            <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full"  name="first_aid_cases" type="number"  required="required"  >
+                        </div>
+
+                        <div>
+                            <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" >Lost time Injuries Free Days</label>
+                                <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full" name="lost_time_injuries_free_days" type="number" required="required" >            
+                        </div>
+
+                        <div>
+                            <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" >Safe men-hours</label>
+                                <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full" name="safe_men_hours" type="number" required="required" >            
+                        </div>
+
+                        <div class="flex items-center gap-4">
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">            Save</button>
+
+                        </div>
+                    </form>
+                </section>
                 </div>
             </div>
         </div>
