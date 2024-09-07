40a3fc2c72ff8fa2d85ee8d730feb67fafc380e4
Maxim Abylkassov <m.abylkassov@gmail.com>
2024-09-07 14:07:51 +0500

    refresh_page_time added to gateboard.blade.php

--- resources/views/admin/gateboard.blade.php
+++ resources/views/admin/gateboard.blade.php
@@ -53,11 +53,19 @@
                                 <textarea class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full" name="running_string" required="required">{{ $lastRecord->running_string ?? '' }}</textarea>            
                         </div>
 
+                        <div>
+                            <label class="block font-medium text-sm text-gray-700 dark:text-gray-300" for="refresh_page_time">Choose the value: <div id="tooltip" class="tooltip" style="display: none;"></div></label>
+                            <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block w-full" type="range" id="refresh_page_time" name="refresh_page_time" min="60" max="3600">
+                            
+                        </div>
+
 
                         <div class="flex items-center gap-4">
                             <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"> Save</button>
 
                         </div>
+
+                        
                     </form>
                 </section>
                 </div>
@@ -90,7 +98,7 @@
 
                             <label class="custom-file-upload">
                                 <input name="video" type="file" class="file-input"/>
-                                Load video for HSEBoard
+                                Load video for Gateboard
                             </label>
                             
                         </div>
@@ -122,6 +130,22 @@
             }
         }, 5000); // 5000 миллисекунд = 5 секунд
     });
+
+    const refresh_page_time = document.getElementById('refresh_page_time');
+        const tooltip = document.getElementById('tooltip');
+
+        refresh_page_time.addEventListener('input', function() {
+            tooltip.style.display = 'inline-block';
+            tooltip.textContent = refresh_page_time.value;
+            const rect = refresh_page_time.getBoundingClientRect();
+            const tooltipRect = tooltip.getBoundingClientRect();
+            tooltip.style.left = `${rect.left + (refresh_page_time.value - refresh_page_time.min) / (refresh_page_time.max - refresh_page_time.min) * rect.width - tooltipRect.width / 2}px`;
+            tooltip.style.top = `${rect.top - tooltipRect.height - 10}px`;
+        });
+
+        refresh_page_time.addEventListener('blur', function() {
+            tooltip.style.display = 'none';
+        });
 </script>
 
 
